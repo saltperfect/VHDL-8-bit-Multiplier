03cdf6f6e8798ef913501d18fdb210d962df6bca
Andrew Staffieri <a.staffieri@hotmail.com>
2011-11-29 18:32:54 -0500

    Removed some inputs to the entity.

--- multiplier.vhd
+++ multiplier.vhd
@@ -4,8 +4,8 @@ use IEEE.std_logic_arith.all;
 
 entity multiplier is
 	port ( mcand, mplier: in STD_LOGIC_VECTOR (7 downto 0);
-	opCode, writeCode, shiftRight: in STD_LOGIC;
-	hilo: in STD_LOGIC_VECTOR (15 downto 0)
+	reset: in STD_LOGIC;
+	product: STD_LOGIC_VECTOR (7 downto 0)
 	);
 end entity;
 
@@ -30,14 +30,16 @@ port( writeflag, shiftflag, addflag: out STD_LOGIC;
 		clk, LO, reset: in STD_LOGIC
 	);
 end component;
-ort(value : out std_logic);
-component clock is
 
+component clock is
+	port(value : out std_logic);
 end component;
 
-signal product: STD_LOGIC_VECTOR (15 downto 0);
+signal product: STD_LOGIC_VECTOR (7 downto 0);
 signal writeflagsignal, shiftflagsignal, addflagsignal, clocksig, LSB, resetsignal, loadsignal: STD_LOGIC;
 begin
+
+	resetsignal <= reset;
 	CLOCK: clock port map(value <= clocksig);
 	M1: control port map (writeflag <= writeflagsignal, shiftflag <= shiftflagsignal, addflag <= addflagsignal, clk <= clocksig, LO <= LSB, reset <= resetsignal);
 	M2: hiloregister port map(mcand <= mcand, HI <= mcand, LO <= mplier, testbit <= LSB, shiftright <= shiftrightsignal, reset <= resetsignal, load <= loadsignal);
